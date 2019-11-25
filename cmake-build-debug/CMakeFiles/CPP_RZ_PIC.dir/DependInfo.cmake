1ea651624efd706e752b672f2f2e747f51deffa8
SmirnovVladimir95 <vladimir.smirnov@phystech.edu>
2019-11-25 17:32:03 +0300

    Add cmake file

---
+++ cmake-build-debug/CMakeFiles/CPP_RZ_PIC.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/Field/Field.cpp" "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/cmake-build-debug/CMakeFiles/CPP_RZ_PIC.dir/Field/Field.cpp.o"
+  "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/Field/PoissonSolver.cpp" "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/cmake-build-debug/CMakeFiles/CPP_RZ_PIC.dir/Field/PoissonSolver.cpp.o"
+  "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/Grid/Grid.cpp" "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/cmake-build-debug/CMakeFiles/CPP_RZ_PIC.dir/Grid/Grid.cpp.o"
+  "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/Particles/Interpolation.cpp" "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/cmake-build-debug/CMakeFiles/CPP_RZ_PIC.dir/Particles/Interpolation.cpp.o"
+  "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/Particles/Particles.cpp" "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/cmake-build-debug/CMakeFiles/CPP_RZ_PIC.dir/Particles/Particles.cpp.o"
+  "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/Particles/Pusher.cpp" "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/cmake-build-debug/CMakeFiles/CPP_RZ_PIC.dir/Particles/Pusher.cpp.o"
+  "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/Tools/Matrix.cpp" "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/cmake-build-debug/CMakeFiles/CPP_RZ_PIC.dir/Tools/Matrix.cpp.o"
+  "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/main.cpp" "/Users/vladimirsmirnov/Desktop/CPP_RZ_PIC_Serial/cmake-build-debug/CMakeFiles/CPP_RZ_PIC.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CPP_RZ_PIC.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_RZ_PIC.dir/Field/Field.cpp.o"
+  "CMakeFiles/CPP_RZ_PIC.dir/Field/PoissonSolver.cpp.o"
+  "CMakeFiles/CPP_RZ_PIC.dir/Grid/Grid.cpp.o"
+  "CMakeFiles/CPP_RZ_PIC.dir/Particles/Interpolation.cpp.o"
+  "CMakeFiles/CPP_RZ_PIC.dir/Particles/Particles.cpp.o"
+  "CMakeFiles/CPP_RZ_PIC.dir/Particles/Pusher.cpp.o"
+  "CMakeFiles/CPP_RZ_PIC.dir/Tools/Matrix.cpp.o"
+  "CMakeFiles/CPP_RZ_PIC.dir/main.cpp.o"
+  "CPP_RZ_PIC"
+  "CPP_RZ_PIC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_RZ_PIC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
