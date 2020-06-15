8896a63c484132e1e6f5a227db73d9fdad3a5a9f
SmirnovVladimir95 <vladimir.smirnov@phystech.edu>
2020-06-15 15:14:30 +0300

    add particle logger

--- Tools/ParticlesLogger.h
+++ Tools/ParticlesLogger.h
@@ -13,15 +13,15 @@ private:
     string observerID;
     string mean_energy_file, velocity_file, position_file, n_total_file;
     ofstream mean_energy_output, velocity_output, position_output, n_total_output;
-    bool iter_check(int iter, int step);
+    static bool iter_check(int iter, int step);
 public:
     ParticlesLogger(Particles& particles, const string& observerID);
     ~ParticlesLogger();
-    void mean_energy_log(int iter = 0, int step = 1, unsigned int mode = ios::app);
-    void velocity_log(int iter = 0, int step = 1, unsigned int mode = ios::app);
-    void position_log(int iter = 0, int step = 1, unsigned int mode = ios::app);
-    void n_total_log(int iter = 0, int step = 1, unsigned int mode = ios::app);
-    void log(int iter = 0, int step = 1, unsigned int mode = ios::app);
+    void mean_energy_log(int iter = 0, int step = 1);
+    void velocity_log(int iter = 0, int step = 1, const vector<int>& ptcls_idx = vector<int>());
+    void position_log(int iter = 0, int step = 1, const vector<int>& ptcls_idx = vector<int>());
+    void n_total_log(int iter = 0, int step = 1);
+    void log(int iter = 0, int step = 1);
 };
 
 
