b3811386f0b88892e9f1cfe7c4465f110d4aca97
SmirnovVladimir95 <vladimir.smirnov@phystech.edu>
2020-06-15 15:45:07 +0300

    fix algorythm in emission

--- InteractionWithMaterial/ParticleEmission.cpp
+++ InteractionWithMaterial/ParticleEmission.cpp
@@ -14,11 +14,12 @@ ParticleEmission::ParticleEmission(Particles &incident_particles, Particles &emi
                                    emission_energy(emission_energy) {}
 
 void ParticleEmission::emission(int seed) {
-    int Ntot = incident_particles->get_Ntot();
-    default_random_engine generator(seed);
+    random_device rd;
+    default_random_engine generator(rd());
     uniform_real_distribution<scalar> distribution(0.0,1.0);
-    scalar vel_module = sqrt(2*emission_energy/emitted_particles->get_mass()), ptcl_z, ptcl_r;
-    for (int ptcl_idx = 0; ptcl_idx < Ntot; ptcl_idx++) {
+    scalar vel_module = sqrt(2*emission_energy/emitted_particles->get_mass());
+    int ptcl_idx = 0;
+    while (ptcl_idx < incident_particles->get_Ntot()) {
         if (emission_condition(ptcl_idx)) {
             if (distribution(generator) < gamma) {
                 array<scalar, 2> pos = incident_particles->get_position(ptcl_idx);
@@ -26,8 +27,10 @@ void ParticleEmission::emission(int seed) {
                                         emission_direction[1]*vel_module,
                                         emission_direction[2]*vel_module};
                 emitted_particles->append(pos, vel);
-                incident_particles->pop(ptcl_idx);
             }
+            incident_particles->pop(ptcl_idx);
+        } else {
+            ptcl_idx++;
         }
     }
 }
@@ -36,10 +39,7 @@ bool ParticleEmission::emission_condition(int ptcl_idx) {
     int cell_z, cell_r;
     const size_t dim = 2;
     array<scalar, dim> pos = incident_particles->get_position(ptcl_idx);
-    //array<scalar, dim> pos = {incident_particles->z[ptcl_idx], incident_particles->r[ptcl_idx]};
     cell_z = floor(pos[0] / grid->dz);
     cell_r = floor(pos[1] / grid->dr);
-    //cell_z = floor(ptcl_z / grid->dz);
-    //cell_r = floor(ptcl_r / grid->dr);
     return domain_condition->operator()(cell_z, cell_r);
 }
